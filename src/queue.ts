51806e2f3e313a42981b64df066cbdeeefa6038d
ykapil897 <ykapil897@gmail.com>
2025-12-20 10:31:27 +0530

    feat: add dead letter queue constants for order and payment processing

--- src/queue.ts
+++ src/queue.ts
@@ -1,2 +1,5 @@
-export const QUEUE_ORDER_CREATED = 'order_created';
+export const QUEUE_ORDER_CREATED = "order_created";
 export const QUEUE_ORDER_CONFIRMED = "order_confirmed";
+
+export const QUEUE_ORDER_DLQ = "order_dlq";
+export const QUEUE_PAYMENT_DLQ = "payment_dlq";
